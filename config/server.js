7b2b6c9629ff37b5dead78149132a328667dc971
Felipe de Lima Lopes <felipe.lopes@pampa.compasso>
2021-07-05 14:10:02 -0300

    innitial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2149a69f646e3bca6f0344ea264a3227'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '89688735-2a25-4c8f-82a0-70a990d75958'
+};
